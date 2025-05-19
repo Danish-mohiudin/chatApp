47f94593b070ac3ddefa5211138ec0b9f58e44a8
Danish Mohiudin <danishmohiudin14@gmail.com>
2025-05-19 22:56:36 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 5000;
 // routes 
 
 import userRoute from './routes/userRoute.js'
-app.use('/user', userRoute)   // This line mounts the entire router userRoute at the path /api/v1/user, or we can say that it is creating the actual route
+app.use('/user', userRoute)  
 
 app.listen(PORT, ()=>{
     console.log(`your server lisening at port ${PORT}`);
