a231255521d4081ea798790d6c113968f83716df
Danish-mohiudin <danishmohiudin14@gmail.com>
2025-08-25 14:31:22 +0530

    ..

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 import {server, app} from './socket/socket.js';
 import express from 'express';
-import coolieParser from 'cookie-parser';
+import cookieParser  from 'cookie-parser';
 import { connectDb } from './db/dbConnectionOne.js';
 import cors from 'cors';
 
@@ -13,7 +13,7 @@ app.use(
     })
 );
 app.use(express.json());
-app.use(coolieParser());
+app.use(cookieParser ());
 //app.use(express.urlencoded({ extended: true }));
 const PORT = process.env.PORT || 5000;
 
