f6cfa7c3e5304b0dc0224c94b1bc3fe546c307b7
Danish-mohiudin <danishmohiudin14@gmail.com>
2025-08-26 13:19:08 +0530

    .

--- server/server.js
+++ server/server.js
@@ -29,6 +29,10 @@ app.use(errorMiddleware);
 app.get("/", (req, res) => {
   res.send("✅ Server is running!");
 });
+app.use((req, res, next) => {
+  console.log("➡️ Incoming request:", req.method, req.url);
+  next();
+});
 
 const PORT = process.env.PORT || 5000;
 server.listen(PORT,"0.0.0.0", ()=>{
